58aeb5a94ea0db48d15e6838196c581460d9f7aa
Devian <info@devian.tk>
2015-02-24 11:22:53 +0100

    Initial Commit

---
+++ Labo 3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Labo 3
+//
+//  Created by Damiaan Dufaux on 24/02/15.
+//  Copyright (c) 2015 Damiaan Dufaux. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
